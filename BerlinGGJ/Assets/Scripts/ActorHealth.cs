7efdc5613cbf312ad8d42a8605eeb60d28ebb84b
Viktor Pekar <vikpek@gmail.com>
2015-01-23 23:57:35 +0100

    wip creep and health

---
+++ BerlinGGJ/Assets/Scripts/ActorHealth.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActorHealth : MonoBehaviour {
+	
+	[SerializeField]
+	float _initialHP = 1f;
+
+	[SerializeField]
+	float _hp = 10f;
+
+
+	public void decreaseHP(float amount)
+	{
+		if((_hp - amount) < 0 )
+		{
+			die ();
+		}
+	}
+
+	public void resteHP()
+	{
+		_hp = _initialHP;
+	}
+
+	public void die()
+	{
+		// TODO implement
+	}
+
+	
+}
---
+++ BerlinGGJ/Assets/Scripts/Creep/CreepCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreepCollision : MonoBehaviour {
+
+	CreepBase creepBase;
+
+	void Awake()
+	{
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if(Tags.PROJECTILE == collision.gameObject.tag){
+
+		}
+	}
+}
