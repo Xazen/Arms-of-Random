bdef6dceaeba98714d31acdaecdf7f7e0e90247d
Dennis Lieu <dennis.lieu@xazen.de>
2015-01-24 12:35:31 +0100

    - Improve item management

--- BerlinGGJ/Assets/Scripts/Item/Item.cs
+++ BerlinGGJ/Assets/Scripts/Item/Item.cs
@@ -14,13 +14,14 @@ public class Item : MonoBehaviour {
 		_playerBase.PlayerCollision.itemCollision += OnPlayerCollision;
 	}
 
-	public void use()
+	public void Use()
 	{
 		if (_itemBase.ItemProperty.CanUseItem ()) 
 		{
 			_itemBase.ItemProperty.DecreasePosessionCount();
+			Debug.Log("weapontype: " + _itemBase.ItemProperty.WeaponType);
 			_playerBase.WeaponController.OnAttack(_itemBase);
-	
+			
 		}
 	}
 
@@ -28,6 +29,7 @@ public class Item : MonoBehaviour {
 	{
 		Debug.Log ("Delete item");
 		_itemBase.ItemVisual.Remove ();
-		Destroy (this.gameObject);
+		this.renderer.enabled = false;
+		this.collider.enabled = false;
 	}
 }
--- BerlinGGJ/Assets/Scripts/Item/ItemProperty.cs
+++ BerlinGGJ/Assets/Scripts/Item/ItemProperty.cs
@@ -5,7 +5,13 @@ public class ItemProperty : MonoBehaviour {
 
 	[SerializeField] private SpriteRenderer slotImage;
 	[SerializeField] private int posessionCount = 0;
+	[SerializeField] private int initialUses = 2;
 	public int WeaponType { get; set; }
+
+	void Start()
+	{
+		posessionCount = initialUses;
+	}
 
 	public void DecreasePosessionCount()
 	{
