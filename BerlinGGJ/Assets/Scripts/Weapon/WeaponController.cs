29086489d81f14f80b7d973094d34d5ef6bba66f
Viktor Pekar <vikpek@gmail.com>
2015-01-24 03:09:36 +0100

    - rough shooting

--- BerlinGGJ/Assets/Scripts/Weapon/WeaponController.cs
+++ BerlinGGJ/Assets/Scripts/Weapon/WeaponController.cs
@@ -2,11 +2,22 @@
 using System.Collections;
 
 public class WeaponController : MonoBehaviour {
+
+
+	PlayerBase _playerBase;
+
 	void Start () {
-	
+		_playerBase = GetComponent<PlayerBase>();
+		_playerBase.PlayerInput.attack += OnAttack;
 	}
 
-	void Update () {
-		
+	void OnAttack()
+	{
+		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube.AddComponent<Rigidbody>();
+		cube.transform.position = transform.position;
+		cube.AddComponent<ConstantForce>();
+		cube.transform.constantForce.force = Vector3.left * 100;
+		cube.transform.Translate( Vector3.forward);
 	}
 }
