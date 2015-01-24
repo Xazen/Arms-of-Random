616a5b2fada51bf0dea3e608eac5d586c7851c38
Dennis Lieu <dennis.lieu@xazen.de>
2015-01-24 08:13:44 +0100

    - Add spawn point and crappy camera

--- BerlinGGJ/Assets/Scripts/CameraMovement.cs
+++ BerlinGGJ/Assets/Scripts/CameraMovement.cs
@@ -3,16 +3,19 @@ using System.Collections;
 
 public class CameraMovement : MonoBehaviour {
 
+	private Transform playerOrigin;
+
 	// Use this for initialization
 	void Start () {
 		GameObject.FindGameObjectWithTag (Tags.PLAYER).GetComponent<PlayerMovement> ().OnPositionChanged += OnPlayerPositionChanged;
+		playerOrigin = GameObject.FindGameObjectWithTag (Tags.PLAYER).transform;
 	}
 	
 	// Update is called once per frame
 	void OnPlayerPositionChanged(Vector3 position)
 	{
 		Vector3 newCameraPosition = this.transform.position;
-		newCameraPosition.x = position.x;
+		newCameraPosition.x = position.x+ 12;
 		this.transform.position = newCameraPosition;
 	}
 }
---
+++ BerlinGGJ/Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPoint : MonoBehaviour {
+
+	[SerializeField] GameObject[] spawnObjects;
+	[SerializeField] Transform spawnPoint;
+	bool activated = false;
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if (collider.gameObject.tag == Tags.PLAYER)
+		{
+			if (!activated)
+			{
+				for(int i = 0; i < spawnObjects.Length; i++)
+				{
+					Vector3 actualSpawnPoint = spawnPoint.position;
+					actualSpawnPoint.x += 5*i;
+					Instantiate((GameObject)spawnObjects[i], actualSpawnPoint, ((GameObject)spawnObjects[i]).transform.rotation);
+				}
+			}
+			activated = true;
+		}
+	}
+}
