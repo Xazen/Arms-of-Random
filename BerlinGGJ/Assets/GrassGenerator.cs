a7840c3eb0644a76e5af5a81599a1458d84303ae
Viktor Pekar <vikpek@gmail.com>
2015-01-24 21:01:10 +0100

    - yooooooooooo

---
+++ BerlinGGJ/Assets/GrassGenerator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrassGenerator : MonoBehaviour {
+
+
+
+	[SerializeField]
+	float _grassDensityAmount = 20;
+
+
+	// Use this for initialization
+	void Start () {
+		Object o  = Resources.Load("Grass/foilage1");
+		Debug.Log("");
+
+				for (int i = 0; i < _grassDensityAmount; i++) {
+	
+				}
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
