c5d4539ac6ac4c9fbb7582e4f82833bb7ef50b8d
Dennis Lieu <dennis.lieu@xazen.de>
2015-01-24 03:28:14 +0100

    - Start implementing item feature

--- BerlinGGJ/Assets/Scripts/Player/PlayerInput.cs
+++ BerlinGGJ/Assets/Scripts/Player/PlayerInput.cs
@@ -5,6 +5,7 @@ public class PlayerInput : MonoBehaviour {
 
 	public delegate void InputDelegate();
 	public delegate void InputDelegateWithValue(float value);
+	public delegate void InputDelegateWithInteger (int value);
 
 	#pragma warning disable
 	private PlayerBase _playerBase;
@@ -13,6 +14,7 @@ public class PlayerInput : MonoBehaviour {
 	public InputDelegate attack;
 	public InputDelegateWithValue moveHorizontal;
 	public InputDelegateWithValue moveVertical;
+	public InputDelegateWithInteger number;
 
 	// Use this for initialization
 	void Start () {
@@ -21,7 +23,23 @@ public class PlayerInput : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown ("Jump")) 
+
+		if (Input.GetKeyDown (KeyCode.Alpha1) || Input.GetKeyDown (KeyCode.Keypad1)) 
+		{
+			number(1);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Alpha2) || Input.GetKeyDown (KeyCode.Keypad2)) 
+		{
+			number(2);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Alpha3) || Input.GetKeyDown (KeyCode.Keypad3)) 
+		{
+			number(3);
+		}
+
+		if (Input.GetButtonDown ("Jump"))
 		{
 			if (jump != null) 
 			{
---
+++ BerlinGGJ/Assets/Scripts/Player/PlayerInventory.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInventory : MonoBehaviour {
+
+	private PlayerBase _playerBase;
+	[SerializeField] private GameObject[] itemList;
+
+	// Use this for initialization
+	void Start () {
+		_playerBase = GetComponent<PlayerBase> ();
+		_playerBase.PlayerInput.number += ItemSelectedWithIndex;
+	}
+
+	public void ItemSelectedWithIndex(int index)
+	{
+		Debug.Log ("Item selected: " + index);
+	}
+}
