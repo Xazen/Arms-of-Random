d1fb5e7d98018a25271d18335ead40bb696fcef5
Dennis Lieu <dennis.lieu@xazen.de>
2015-01-25 05:32:03 +0100

    - Environment improvement

--- BerlinGGJ/Assets/BackgroundMovement.cs
+++ BerlinGGJ/Assets/BackgroundMovement.cs
@@ -1,20 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraMovement : MonoBehaviour {
+public class BackgroundMovement : MonoBehaviour {
 
 	private Transform playerOrigin;
-
+	
 	void Start () {
 		GameObject.FindGameObjectWithTag (Tags.PLAYER).GetComponent<PlayerMovement> ().OnPositionChanged += OnPlayerPositionChanged;
 		playerOrigin = GameObject.FindGameObjectWithTag (Tags.PLAYER).transform;
 	}
-
+	
 	void OnPlayerPositionChanged(Vector3 position)
 	{
 		Vector3 newCameraPosition = this.transform.position;
-		newCameraPosition.x = position.x + 12;
-		newCameraPosition.y = position.y + 17;
+		newCameraPosition.x = (float) position.x * 1.5f + 12;
 		this.transform.position = newCameraPosition;
 	}
 }
